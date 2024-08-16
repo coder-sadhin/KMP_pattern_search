6da11afe4a503de912d41945c8fa69f8da4f38b5
coder-sadhin <codersadhin@gmail.com>
2024-08-16 12:30:33 +0600

    pattern search function

--- KMP_pattern_search.py
+++ KMP_pattern_search.py
@@ -16,4 +16,18 @@ def KMP_pattern_search(pattern, text):
     # index for text[]
     i=0
 
-    
+    while i < Q:
+        if pattern[j] == text[i]:
+            i += 1
+            j += 1
+
+        if j == P:
+            print("Pattern found at index " + str(i-j))
+            j=long_prefix_suffix[j-1]
+
+        # mismatch after j matches
+        elif i < Q and pattern[j] != text[i]:
+            if j != 0:
+                j=long_prefix_suffix[j-1]
+            else:
+                i += 1
