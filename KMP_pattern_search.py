f91c7548b0f392e464b5540198a16139d8c9365d
coder-sadhin <codersadhin@gmail.com>
2024-08-16 12:29:35 +0600

    kmp patarn

---
+++ KMP_pattern_search.py
@@ -0,0 +1,17 @@
+#Python program for KMP Algorithm
+def KMP_pattern_search(pattern, text):
+    P = len(pattern)
+    Q = len(text)
+
+    # create long_prefix_suffix[] that will hold the longest prefix suffix
+    # values for pattern
+    long_prefix_suffix = [0]*P
+
+    # index for pattern[]
+    j=0
+
+    # preprocess the pattern (caluclate long_prefix_suffix[] array)
+    long_Prefix_Suffix_Array(pattern, P, long_prefix_suffix)
+
+    # index for text[]
+    i=0
